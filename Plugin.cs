87c3b0b66c72b23104982b012c9cd5a8e4b86ad3
Víctor Villanueva <vicvinue@vicvinue.local>
2026-05-14 14:55:33 -0400

    Fix missing using MediaBrowser.Model.Plugins for BasePluginConfiguration

--- Plugin.cs
+++ Plugin.cs
@@ -1,6 +1,7 @@
 using System;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
 namespace Jellyfin.Plugin.SubtitleCat;
